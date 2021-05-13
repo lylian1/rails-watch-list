4764c943774b3f1048bab665c7da9456f7e75ed1
lylian krizoua <krizoualylian@gmail.com>
2021-05-13 14:13:10 +0200

    validation

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+    validates :name, presence: true
+    has_many :bookmarks, dependent: :destroy
+    has_many :movies, through: :bookmarks
+    validates :name, presence: true, uniqueness: true
+end

cf43b4b12ccc69f54ba76072a58057b0183e4b1b
lylian krizoua <krizoualylian@gmail.com>
2021-05-14 12:25:09 +0200

    fix csss

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const { environment } = require('@rails/webpacker')
+//const { environment } = require('@rails/webpacker')
 
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 const webpack = require('webpack')
@@ -11,6 +11,6 @@ environment.plugins.prepend('Provide',
   })
 )
 
-module.exports = environment
+//module.exports = environment
 
 module.exports = environment
